954fce380daecaddfa1c148463054e5e930717f1
abacoProgr <theabaco87@gmail.com>
2019-12-20 14:03:52 +0100

    Aggiunta schermata avvio app con logo

---
+++ app/src/main/java/com/agelm/legomine/SplashScreen.java
@@ -0,0 +1,29 @@
+package com.agelm.legomine;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+
+public class SplashScreen extends Activity {
+
+    int SPLASH_TIME_OUT = 3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logo);
+        ImageView iv= (ImageView) findViewById(R.id.imageView);
+        iv.setImageResource(R.drawable.logo_legomine);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
